8f867c461e33f60ecd4538b1252d8188ce79bd51
Victor Batigin <bixs85@gmail.com>
2020-03-06 22:44:54 +0200

    add 7 task

--- js/task-07.js
+++ js/task-07.js
@@ -1,4 +1,12 @@
 console.log(`Task 7`);
 
 const inputRef = document.querySelector('input[id="font-size-control"]');
-console.log(inputRef);
+const spanRef = document.querySelector('span[id="text"]');
+
+inputRef.addEventListener("input", onRange);
+spanRef.classList.add("fsize");
+
+function onRange(event) {
+  spanRef.style.fontSize = event.target.value + "px";
+  return;
+}
