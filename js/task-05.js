4ef86f333ac493110f41074bc26d8f55feffe0de
Victor Batigin <bixs85@gmail.com>
2020-03-06 21:37:10 +0200

    add 5 and 6 task

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,13 @@
 console.log(`Task 5`);
+
+const inputRef = document.querySelector('input[id="name-input"]');
+const spanRef = document.querySelector('span[id="name-output"]');
+
+inputRef.addEventListener("input", onName);
+
+function onName(event) {
+  if (event.target.value === "") {
+    return (spanRef.textContent = "незнакомец");
+  }
+  return (spanRef.textContent = event.target.value);
+}
--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,20 @@
 console.log(`Task 6`);
+
+const inputRef = document.querySelector('input[id="validation-input"]');
+
+inputRef.addEventListener("focus", onFocus);
+inputRef.addEventListener("blur", onblur);
+
+function onFocus(event) {
+  inputRef.classList.remove("valid");
+  inputRef.classList.remove("invalid");
+}
+
+function onblur(event) {
+  if (event.target.value.length === Number(event.target.dataset.length)) {
+    inputRef.classList.add("valid");
+    return;
+  }
+  inputRef.classList.add("invalid");
+  return;
+}
