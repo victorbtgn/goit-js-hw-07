b954407df7c2074b888caf22d4ce80cc19d3d515
Victor Batigin <bixs85@gmail.com>
2020-03-06 13:12:31 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,22 @@
 console.log(`Task 2`);
+
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы"
+];
+
+const ulRef = document.querySelector("#ingredients");
+
+const productListTag = array =>
+  array.map(item => {
+    const product = document.createElement("li");
+    product.textContent = item;
+    return ulRef.appendChild(product);
+  });
+
+console.log(ulRef);
+console.log(productListTag(ingredients));
